c657174797de62e5f400d72f67a2145fcfcc9156
meredithhall716 <meredith.hall.oboe@gmail.com>
2022-06-01 20:43:09 -0400

    first

--- guessing_game.py
+++ guessing_game.py
@@ -1,1 +1,20 @@
-class GuessingGame():
+class GuessingGame:  
+
+    def __init__(self, answer_number):
+        self.answer_number = 10
+        self.is_solved = False
+   
+    def guess(self, user_guess):
+        if user_guess == self.answer_number:
+            self.is_solved = True
+            return 'correct'
+        elif user_guess < self.answer_number:
+            return 'low'
+        else: return 'high'
+    
+    def solved(self):
+        return self.is_solved
+    
+game = GuessingGame(10)
+
+print(game.guess(10))
